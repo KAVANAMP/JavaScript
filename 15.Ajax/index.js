d18ff92f003381e1bcfcf8b8156d641a54b2d6fc
KAVANAMP <153711029+KAVANAMP@users.noreply.github.com>
2026-07-05 07:42:44 +0530

    new

--- 15.Ajax/index.js
+++ 15.Ajax/index.js
@@ -1,3 +1,31 @@
+//AJAX - AJAX (Asynchronous JavaScript and XML) is a technique used to send and receive data from a server asynchronously without reloading the entire webpage. -or- without reloading it updates the page.
+
+//Synchronous - Executes line by line
+//Asynchronous - Does not wait for completion or do not block the execution of the code.
+
+//Technologies Used in AJAX
+/*AJAX is not a programming language.
+
+It is a combination of:
+
+HTML
+CSS
+JavaScript
+DOM
+JSON/XML
+HTTP
+*/
+
+//Ways to Perform AJAX
+/*
+There are three main ways:
+
+1.XMLHttpRequest (Old)
+2.Fetch API (Modern)
+3.Axios (Library)
+*/
+
+
 function loads(){
      let request=new XMLHttpRequest()
      request.open('GET','./index.txt')
